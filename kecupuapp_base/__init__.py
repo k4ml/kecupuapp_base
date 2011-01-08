1707b51f0e9cadeb4d17bbc2f8439b2d2ef1695c
Mohd Kamal Bin Mustafa <kamal.mustafa@gmail.com>
2011-01-08 14:42:26 +0800

    Initial

---
+++ kecupuapp_base/__init__.py
@@ -0,0 +1,39 @@
+from django.http import HttpResponse
+
+class BaseSite:
+
+    def __init__(self, name=None, app_name='kecupuapp_base'):
+        if name is None:
+            self.name = 'kecupuapp_base'
+        else:
+            self.name = name
+        self.app_name = name
+
+    def login(self, request):
+        from django.contrib.auth.views import login
+        return login(request, template_name='kecupuapp_base/login.html')
+
+    def logout(self, request):
+        from django.contrib.auth.views import logout_then_login
+        return logout_then_login(request)
+
+    def profile(self, request):
+        from kecupuapp_base.shortcuts import render_response 
+        return render_response(request, 'kecupuapp_base/profile.html')
+
+    def get_urls(self):
+        from django.conf.urls.defaults import patterns, url, include
+
+        urlpatterns = patterns('',
+            url(r'^login/$', self.login, name='login'),
+            url(r'^logout/$', self.logout, name='logout'),
+            url(r'^profile/$', self.profile, name='profile'),
+        )
+
+        return urlpatterns
+
+    def urls(self):
+        return self.get_urls(), self.app_name, self.name
+    urls = property(urls)
+    
+basesite = BaseSite()
---
+++ kecupuapp_base/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import *
+
+# URL patterns for kecupu.base
+
+urlpatterns = patterns('kecupu.base.views',
+  # Add url patterns here
+)
