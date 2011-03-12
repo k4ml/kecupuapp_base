c6c6f844e8679f1219efe805089da8f247ee7c41
Mohd Kamal Bin Mustafa <kamal.mustafa@gmail.com>
2011-03-12 15:29:35 +0800

    login required profile page

--- kecupuapp_base/views.py
+++ kecupuapp_base/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.contrib.auth.views import login as auth_login
 from django.contrib.auth.views import logout_then_login
+from django.contrib.auth.decorators import login_required
 
 from kecupuapp_base.shortcuts import render_response 
 
@@ -10,5 +11,6 @@ def login(request):
 def logout(request):
     return logout_then_login(request)
 
+@login_required
 def profile(request):
     return render_response(request, 'kecupuapp_base/profile.html')
