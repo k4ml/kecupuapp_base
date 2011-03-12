c0b431b1ce6b74c464d10f40e2a2d9b4cd438ad4
Mohd Kamal Bin Mustafa <kamal.mustafa@gmail.com>
2011-03-12 15:16:35 +0800

    use function based views instead of class

--- kecupuapp_base/urls.py
+++ kecupuapp_base/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls.defaults import *
 
 # URL patterns for kecupu.base
 
-urlpatterns = patterns('kecupu.base.views',
-  # Add url patterns here
+urlpatterns = patterns('kecupuapp_base.views',
+    # Add url patterns here
+    url(r'^login/$', 'login', name='login'),
+    url(r'^logout/$', 'logout', name='logout'),
+    url(r'^profile/$', 'profile', name='profile'),
 )
--- kecupuapp_base/views.py
+++ kecupuapp_base/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+from django.contrib.auth.views import login as auth_login
+from django.contrib.auth.views import logout_then_login
+
+from kecupuapp_base.shortcuts import render_response 
+
+def login(request):
+    return auth_login(request, template_name='kecupuapp_base/login.html')
+
+def logout(request):
+    return logout_then_login(request)
+
+def profile(request):
+    return render_response(request, 'kecupuapp_base/profile.html')
