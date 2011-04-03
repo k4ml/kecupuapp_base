825b3e81144e49f88bfd4b23c28ef4395a16c84e
Kamal Bin Mustafa <kamal.mustafa@gmail.com>
2011-04-03 18:26:30 +0000

    implement change password page

--- kecupuapp_base/urls.py
+++ kecupuapp_base/urls.py
@@ -10,4 +10,6 @@ urlpatterns = patterns('kecupuapp_base.views',
     url(r'^login/$', 'login', name='login'),
     url(r'^logout/$', 'logout', name='logout'),
     url(r'^profile/$', 'profile', name='profile'),
+    url(r'^profile/password/change/$', 'password_change', name='password-change'),
+    url(r'^profile/password/change-done/$', 'password_change_done', name='password-change-done'),
 )
